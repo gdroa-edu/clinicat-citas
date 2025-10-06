2aa37bf7505f7525c632a7dc78c067f435d10d41
user.name <gissel.gonzalez@fiuni.edu.py>
2025-10-06 15:57:44 -0300

    Primer commit

---
+++ src/main/java/com/clinicat/citas/ClinicatCitasApplication.java
@@ -0,0 +1,13 @@
+package com.clinicat.citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicatCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicatCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicat/citas/ClinicatCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicat.citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicatCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
